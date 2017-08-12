1e58474bb3fcc186fbbf0868c26ba24c33b8f56f
Brecht Billiet <billietbrecht@gmail.com>
2017-08-12 11:17:27 +0200

    initial setup

---
+++ winecellar/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello world</h1>
+  `,
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'app';
+}
