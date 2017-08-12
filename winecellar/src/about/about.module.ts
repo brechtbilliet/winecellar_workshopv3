76bb2183526a1a47edca6460ad660a62eabe03cd
Brecht Billiet <billietbrecht@gmail.com>
2017-08-12 13:27:49 +0200

    common-ogic

--- winecellar/src/about/about.module.ts
+++ winecellar/src/about/about.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AboutPageContainer } from './containers/about/about-page.container';
-import { CommonLogicModule } from '../common/common.module';
+import { CommonLogicModule } from '../common-logic/common-logic.module';
 
 @NgModule({
   declarations: [
