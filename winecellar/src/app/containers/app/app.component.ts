4b8bd98e8ddc16016432a290a1b6e7506ac22156
Brecht Billiet <billietbrecht@gmail.com>
2017-08-13 15:23:59 +0200

    ngrx implementation of authentication

--- winecellar/src/app/containers/app/app.component.ts
+++ winecellar/src/app/containers/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { WinecellarState } from '../../../statemanagement/state';
 import { AuthenticationService } from '../../../authentication/services/authentication.service';
@@ -8,30 +8,21 @@ import { Router } from '@angular/router';
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
   template: `
-    <app-navbar (logout)="onLogout()" *ngIf="authenticated"></app-navbar>
+    <app-navbar (logout)="onLogout()" *ngIf="authenticated$|async"></app-navbar>
     <router-outlet></router-outlet>
     <app-spinner [spin]="true"></app-spinner>
   `,
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnDestroy {
-  authenticated = false;
-
-  private subscriptions = [];
+export class AppComponent {
+  authenticated$ = this.store.select(state => state.authentication.isAuthenticated);
 
   constructor(private router: Router, private authenticationService: AuthenticationService, private store: Store<WinecellarState>) {
     this.authenticationService.checkInitialAuthentication();
-    this.subscriptions.push(this.store.subscribe((state) => {
-      this.authenticated = state.authentication.isAuthenticated;
-    }));
   }
 
   onLogout(): void {
     this.authenticationService.logout();
     this.router.navigate(['/authentication']);
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
