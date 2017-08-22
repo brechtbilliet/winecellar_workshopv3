2110969c5df2da768f79375d6fbad917439dcc11
Brecht Billiet <billietbrecht@gmail.com>
2017-08-22 19:15:58 +0200

    completed the autocomplete

--- winecellar/src/stock/containers/wine-search/wine-search.container.ts
+++ winecellar/src/stock/containers/wine-search/wine-search.container.ts
@@ -8,13 +8,17 @@ import {
   SimpleChanges
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Product, WineComService } from '../../services/wine-com.service';
-import { Observable } from 'rxjs/Observable';
+import { Product, WineComSearchResult, WineComService } from '../../services/wine-com.service';
 import 'rxjs/add/observable/of';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-wine-search',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./wine-search.container.less'],
   template: `
     <div class="form-group has-feedback" [class.has-success]="control.valid">
       <label for="searchInput" class="col-sm-4 control-label">
@@ -38,7 +42,19 @@ export class WineSearchContainer implements OnChanges {
   @Output() selectWine = new EventEmitter<Product>();
   control = new FormControl('');
 
-  winesToShow$ = Observable.of([]);
+  private showResults$ = new BehaviorSubject(true);
+
+  private clear$ = this.showResults$.filter(val => !val)
+    .map(() => []);
+
+  winesToShow$ = this.control.valueChanges
+    .do((value: string) => this.showResults$.next(false)) // user types, hide the results
+    .filter(value => value.length > 2)
+    .debounceTime(300)
+    .switchMap(value => this.wineComService.search(value))
+    .map((res: WineComSearchResult) => res.products.list)
+    .merge(this.clear$)
+    .distinctUntilChanged();
 
   constructor(private wineComService: WineComService) {
   }
@@ -50,5 +66,6 @@ export class WineSearchContainer implements OnChanges {
 
   onSelectWine(wine: Product): void {
     this.selectWine.emit(wine);
+    this.showResults$.next(false);
   }
 }
